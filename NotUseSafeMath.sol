68680a0a365760946f54637169e64bfe9da1ccb7
dqvinh95 <31183124+dqvinh95@users.noreply.github.com>
2023-10-12 23:50:17 +0700

    Add files via upload

---
+++ NotUseSafeMath.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract useSafeMath{
+//  using  SafeMath for uint256;
+    function number (uint8 a, uint8 b) public pure returns(uint8){
+      return a+b;
+    }
+}
+
+//Use "pure" 
+//In Solidity, a function that doesn’t read or modify the variables of the state is called a pure function. 
+//It can only use local variables that are declared in the function and the arguments that are passed to the function to compute or return a value.
---
+++ SafeMath.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library SafeMath {
+
+  /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
+    }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
+
+  /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+
+contract useSafeMath{
+  using SafeMath for uint256;
+    function number (uint a, uint b) public pure returns(uint){
+      return a.add(b);
+    }
+}
+
+//Use "pure" 
+//In Solidity, a function that doesn’t read or modify the variables of the state is called a pure function. 
+//It can only use local variables that are declared in the function and the arguments that are passed to the function to compute or return a value.
